01c18859af6b19063510f41cbafa9a970a8e429a
Shuṗpo Man <72602326+Shuppin@users.noreply.github.com>
2024-08-06 22:35:19 +0100

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Top Down Shooter with Massive Enemy Hordes
+
+This is a learning project to get a better grasp on Bevy and to explore efficient algorithms for handling massive groups of entities.
+
+It is a reflection of [this](https://www.youtube.com/watch?v=p8d8TKo59LU) tutorial, with some of my own modifications.
+
+## TODO
+- [x] Project init
+- [ ] Sprites for player, enemies, gun and bullets
+- [ ] Game state
+- [ ] Player system (movement, rendering)
+- [ ] Player gun system (Anchored to player, follows mouse)
+- [ ] Bullet system (gun fires bullets)
+- [ ] Map decorations
+- [ ] Enemies (spanwning, follow player)
+- [ ] Animations (player/enemy sprite anims)
+- [ ] Collisions (enemy bullet collisions)
+- [ ] Kd-tree implementation (efficient collisions)
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+};
+
+fn main() {
+    App::new()
+        .insert_resource(Msaa::Off)
+        .add_plugins((
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        resolution: (500.0, 500.0).into(),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                }),
+            LogDiagnosticsPlugin::default(),
+            FrameTimeDiagnosticsPlugin,
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(Update, close_on_esc)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn close_on_esc(keyboard: Res<ButtonInput<KeyCode>>, mut exit: EventWriter<AppExit>) {
+    if keyboard.just_pressed(KeyCode::Escape) {
+        info!("Bye bye!");
+        exit.send(AppExit::Success);
+    }
+}
